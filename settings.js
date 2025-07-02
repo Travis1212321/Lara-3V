f9b92495f552f45691c5761559815faf360ae9ac
Travis1212321 <rekardo647@gmail.com>
2025-07-03 01:47:43 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "GYAExYzZ#6KWRLPV5tE9pO9nuMnkuY42IryhHI54ZQU0U0jEsxNY", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "249126083647",
 DEV: process.env.DEV || "249126083647",
